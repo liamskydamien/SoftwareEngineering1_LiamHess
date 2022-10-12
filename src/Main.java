67514578360dcc29825f157feb3b4e681ef71eaf
liamskydamien <liamskydamien@googlemail.com>
2022-10-12 10:28:21 +0200

    Uebung1 fix

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import Uebung1.view.Client;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Client showCaseClient = new Client();
+        for (Integer i: new int[]{1, 5, 7, 10, 11, -1}) {
+            showCaseClient.display(i);
+        }
     }
 }
