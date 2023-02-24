4fcc99adfea095afc3ca0d3ce051403942f495e8
Liam Hess <liamskydamien@gmail.com>
2023-02-24 16:52:33 +0100

    Add View

--- src/LearningPatterns/SingeltonAndFactoryMethodPattern/View.java
+++ src/LearningPatterns/SingeltonAndFactoryMethodPattern/View.java
@@ -1,4 +1,20 @@
 package LearningPatterns.SingeltonAndFactoryMethodPattern;
 
+import java.util.Date;
+
 public class View {
+    private ComplexClass[] complexClasses = new ComplexClass[5];
+    private final ComplexClassFactory factory = ComplexClassFactory.getInstance();
+
+    public void createClasses(){
+        for (int i = 0; i < 5; i++) {
+            complexClasses[i] = factory.createComplexClass("Liam", new Date("15-08-2001"));
+        }
+    }
+
+    public void displayClasses(){
+        for (ComplexClass c : complexClasses){
+            System.out.println(c.toString());
+        }
+    }
 }
