8bb7bb78943edb78748756284a1057ad6bcd7315
Liam Hess <liamskydamien@gmail.com>
2023-02-24 16:36:24 +0100

    Add Complex Class

--- src/LearningPatterns/SingeltonAndFactoryMethodPattern/ComplexClass.java
+++ src/LearningPatterns/SingeltonAndFactoryMethodPattern/ComplexClass.java
@@ -1,4 +1,18 @@
 package LearningPatterns.SingeltonAndFactoryMethodPattern;
 
+import java.util.Date;
+
 public class ComplexClass {
+    private final String name;
+    private final Date birthday;
+
+    public ComplexClass(String name, Date birthday) {
+        this.name = name;
+        this.birthday = birthday;
+    }
+
+    public String toString(){
+        return "I am " + name + " and I was born on the " + birthday;
+    }
+
 }
