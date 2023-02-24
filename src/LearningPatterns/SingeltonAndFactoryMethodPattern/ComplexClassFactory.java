e102b4dfd9c0c6c4082f7c9b67cb42e44e5854b7
Liam Hess <liamskydamien@gmail.com>
2023-02-24 16:44:57 +0100

    Implement Singleton Pattern

--- src/LearningPatterns/SingeltonAndFactoryMethodPattern/ComplexClassFactory.java
+++ src/LearningPatterns/SingeltonAndFactoryMethodPattern/ComplexClassFactory.java
@@ -1,4 +1,19 @@
 package LearningPatterns.SingeltonAndFactoryMethodPattern;
 
 public class ComplexClassFactory {
+
+    private static ComplexClassFactory instance = null;
+
+    public static synchronized ComplexClassFactory getInstance(){
+        if (instance == null){
+            return new ComplexClassFactory();
+        }
+        return instance;
+    }
+
+    private ComplexClassFactory() {
+        instance = this;
+    }
+
+
 }
