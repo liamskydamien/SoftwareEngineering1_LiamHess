be8227b5a6d174cf8ecc941bb140746c2f3b7347
Liam Hess <liamskydamien@gmail.com>
2023-03-02 18:48:28 +0100

    Setup Strategy Pattern

--- src/LearningPatterns/StrategyPattern/User.java
+++ src/LearningPatterns/StrategyPattern/User.java
@@ -1,4 +1,14 @@
 package LearningPatterns.StrategyPattern;
 
 public class User {
+    private Strategy strategy = null;
+
+    public void setStrategy(Strategy strategy){
+        this.strategy = strategy;
+    }
+
+    public void doSomething(){
+        System.out.println("Do Something" + strategy);
+    }
+
 }
