e7380da114de5d1025c430e4d9e2b0398c02eec0
Liam Hess <liamskydamien@gmail.com>
2023-03-03 20:54:09 +0100

    Add SkillReport

---
+++ src/LearningPatterns/ReportPublisher/SkillReport.java
@@ -0,0 +1,39 @@
+package LearningPatterns.ReportPublisher;
+
+public class SkillReport implements Report {
+    private int id;
+    private String skill;
+    private String student;
+
+    public SkillReport(int id, String skill, String student) {
+        this.id = id;
+        this.skill = skill;
+        this.student = student;
+    }
+
+    public String getSkill() {
+        return skill;
+    }
+
+    public void setSkill(String skill) {
+        this.skill = skill;
+    }
+
+    public String getStudent() {
+        return student;
+    }
+
+    public void setStudent(String student) {
+        this.student = student;
+    }
+
+    @Override
+    public int getID() {
+        return id;
+    }
+
+    @Override
+    public void setID(int id) {
+
+    }
+}
