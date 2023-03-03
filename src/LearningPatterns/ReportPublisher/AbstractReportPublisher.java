54cdad871d72fe5807e5a7c6cb2ee626978c1503
Liam Hess <liamskydamien@gmail.com>
2023-03-03 21:39:16 +0100

    Add CompanyReportPublisher

--- src/LearningPatterns/ReportPublisher/AbstractReportPublisher.java
+++ src/LearningPatterns/ReportPublisher/AbstractReportPublisher.java
@@ -1,10 +1,12 @@
 package LearningPatterns.ReportPublisher;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public abstract class AbstractReportPublisher {
     private List<Consumer> subscribers = new ArrayList<>();
+    final HashMap<Integer, Report> reports = new HashMap<>();
 
     public void register(Consumer consumer){
         subscribers.add(consumer);
--- src/LearningPatterns/ReportPublisher/SkillReportPublisher.java
+++ src/LearningPatterns/ReportPublisher/SkillReportPublisher.java
@@ -5,16 +5,15 @@ import java.util.HashMap;
 public class SkillReportPublisher extends AbstractReportPublisher{
     private int counter = 0;
 
-    private final HashMap<Integer, Report> reports = new HashMap<>();
     @Override
     public void produce() {
         String[] strings = RandomData.getSkillData();
         Report report = new SkillReport(++counter, strings[0], strings[1]);
-        reports.put(counter, report);
+        super.reports.put(counter, report);
     }
 
     @Override
     public Report getReport(int id) {
-        return reports.get(id);
+        return super.reports.get(id);
     }
 }
