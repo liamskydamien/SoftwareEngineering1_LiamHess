beaa203a83dfab5397525d11b02f5f68cf432c17
Liam Hess <liamskydamien@gmail.com>
2023-03-05 17:31:04 +0100

    Adding TextDocument

--- src/LearningPatterns/Composite/TextDocument.java
+++ src/LearningPatterns/Composite/TextDocument.java
@@ -1,4 +1,15 @@
 package LearningPatterns.Composite;
 
-public class TextDocument {
+public class TextDocument extends CoreDocument{
+    private final String text;
+
+    public TextDocument(String text) {
+        super();
+        this.text = text;
+    }
+
+    @Override
+    public void operation() {
+        System.out.println(text);
+    }
 }
